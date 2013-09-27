0469dfe48c5adf6db5e09ffa4cdd97116f66b96b
harun yardimci <harun.yardimci@gmail.com>
2013-09-28 01:36:08 +0300

    -

--- Inspirator/inspiratorViewController.h
+++ Inspirator/inspiratorViewController.h
@@ -8,18 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-@interface inspiratorViewController : UIViewController
+//@interface inspiratorViewController : UIViewController
+
+@interface inspiratorViewController : UIViewController<UITableViewDelegate,UITableViewDataSource> {
+	IBOutlet UITableView *tableview;
+	IBOutlet UIDatePicker *datePicker;
+	IBOutlet UITextField *eventText;
+}
+
+@property (nonatomic, retain) IBOutlet UIDatePicker *datePicker;
+@property (nonatomic, retain) IBOutlet UITableView *tableview;
+@property (nonatomic, retain) IBOutlet UITextField *eventText;
 
 @property (weak, nonatomic) IBOutlet UILabel *lblQuote;
 @property (weak, nonatomic) IBOutlet UILabel *lblAuthor;
 
-- (IBAction)showNextQuote:(id)sender;
-
-
 @property (copy, nonatomic) NSString *author;
 @property (copy, nonatomic) NSString *quote;
-
 @property (copy, nonatomic) NSArray *contentArray;
 
+- (IBAction) showNextQuote:(id)sender;
+- (IBAction) scheduleAlarm2:(id) sender;
+
+- (IBAction) openSettings:(id)sender;
 
 @end
--- Inspirator/inspiratorViewController.m
+++ Inspirator/inspiratorViewController.m
@@ -17,6 +17,10 @@
 @synthesize author = _author;
 @synthesize quote = _quote;
 
+@synthesize datePicker;
+@synthesize tableview;
+@synthesize eventText;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -31,7 +35,23 @@
     
     // show The First Quote
     [self showNextQuote:self];
+    
 }
+
+- (void)viewDidUnload {
+    
+	datePicker = nil;
+	tableview = nil;
+	eventText = nil;
+    
+    _author = nil;
+    _quote = nil;
+    
+    self.author = nil;
+    self.quote = nil;
+    
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -39,12 +59,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 - (void) loadQuotesFromFile {
     
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"quotes" ofType:@"plist"];
     self.contentArray = [NSArray arrayWithContentsOfFile:filePath];
-    
 }
 
 
@@ -54,9 +72,7 @@
     
     self.lblQuote.text = [self getQuote:randomNumber];
     self.lblAuthor.text = [self getAuthor:randomNumber];
-    
 }
-
 
 - (int) getRandomNumber {
     return arc4random() % [self.contentArray count];
@@ -66,9 +82,9 @@
 
     NSDictionary *dict = [self.contentArray objectAtIndex:random];
     self.quote = (NSString*)[dict objectForKey:@"quote"];
-    //self.quote = @"The best way to predict the future is to invent it.";
     
-    NSString *quoteText = [[NSString alloc] initWithFormat:@"\"%@\"", self.quote];
+    // NSString *quoteText = [[NSString alloc] initWithFormat:@"\"%@\"", self.quote];
+    NSString *quoteText = [[NSString alloc] initWithFormat:@"%@", self.quote];
     
     return quoteText;
 }
@@ -77,12 +93,90 @@
 
     NSDictionary *dict = [self.contentArray objectAtIndex:random];
     self.author = (NSString*)[dict objectForKey:@"author"];
-    //self.author = @"Alan Kay";
     
     NSString *authorText = [[NSString alloc] initWithFormat:@"- %@", self.author];
     
     return authorText;
 }
+
+- (IBAction) openSettings:(id)sender {
+
+    UIViewController* settingsViewController = [[UIViewController alloc] initWithNibName:@"Settings" bundle:[NSBundle mainBundle]];
+    [self.view addSubview:settingsViewController.view];
+}
+
+
+- (IBAction) scheduleAlarm:(id) sender {
+//    [eventText resignFirstResponder];
+//    
+//    NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar];
+//    
+//    // Get the current date
+//    NSDate *pickerDate = [self.datePicker date];
+//    
+//    // Break the date up into components
+//    NSDateComponents *dateComponents = [calendar components:( NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit )
+//												   fromDate:pickerDate];
+//    NSDateComponents *timeComponents = [calendar components:( NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit )
+//												   fromDate:pickerDate];
+//    // Set up the fire time
+//    NSDateComponents *dateComps = [[NSDateComponents alloc] init];
+//    [dateComps setDay:[dateComponents day]];
+//    [dateComps setMonth:[dateComponents month]];
+//    [dateComps setYear:[dateComponents year]];
+//    [dateComps setHour:[timeComponents hour]];
+//	// Notification will fire in one minute
+//    [dateComps setMinute:[timeComponents minute]];
+//	[dateComps setSecond:[timeComponents second]];
+//    NSDate *itemDate = [calendar dateFromComponents:dateComps];
+//    [dateComps release];
+//    
+//    UILocalNotification *localNotif = [[UILocalNotification alloc] init];
+//    if (localNotif == nil)
+//        return;
+//    localNotif.fireDate = itemDate;
+//    localNotif.timeZone = [NSTimeZone defaultTimeZone];
+//    
+//	// Notification details
+//    localNotif.alertBody = [eventText text];
+//	// Set the action button
+//    localNotif.alertAction = @"View";
+//    
+//    localNotif.soundName = UILocalNotificationDefaultSoundName;
+//    localNotif.applicationIconBadgeNumber = 1;
+//    
+//	// Specify custom data for the notification
+//    NSDictionary *infoDict = [NSDictionary dictionaryWithObject:@"someValue" forKey:@"someKey"];
+//    localNotif.userInfo = infoDict;
+//    
+//	// Schedule the notification
+//    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
+//    [localNotif release];
+    
+//	[self.tableview reloadData];
+}
+
+
+
+
+//- (void)addNotification {
+//    UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+//    
+//    localNotification.fireDate = [NSDate date];
+//    localNotification.alertBody = @"get your daily inspiration";
+//    localNotification.soundName = UILocalNotificationDefaultSoundName;
+//    localNotification.applicationIconBadgeNumber = 1;
+//    
+//    NSDictionary *infoDict = [NSDictionary dictionaryWithObjectsAndKeys:@"Object 1", @"Key 1", @"Object 2", @"Key 2", nil];
+//    localNotification.userInfo = infoDict;
+//    
+//    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+//    [localNotification release];
+//}
+
+//- (void) dealloc {
+//    [super dealloc];
+//}
 
 
 @end
